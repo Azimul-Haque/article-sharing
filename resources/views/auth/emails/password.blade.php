7e1674fefc23dacb1eddad3a198134adadf43760
Azimul Haque <orbachinujbuk@gmail.com>
2021-02-16 00:51:17 +0600

    ASD324

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -2,7 +2,7 @@
   <div style="border: 1px solid #ACACAC; padding: 10px; max-width: 500px;">
     <center>
       <img src="{{ asset('images/logo.png') }}" width="100px; height: auto;">
-      <p style="font-size: 30px; color: #1B237D;"><b>Killa</b>Consultancy</p>
+      <p style="font-size: 30px; color: #1B237D;"><b>Ten</b><big>X</big></p>
       <p style="font-size: 25px"><b>Your Password Reset Link</b></p>
     
       <p style="font-size: 20px">
@@ -16,7 +16,7 @@
         This is a auto-generated email from Killa Consultancy. This email arrived to you because you (or may be someone else!) have requested to reset the password associated with this email address. If you are getting this email by mistake, please ignore it.
       </p>
       <p style="font-size: 12px; color: #ACACAC;">
-        &copy; @php echo date('Y'); @endphp <a href="http://killabd.com/">Killa Consultancy</a>, Mirpur, Dhaka, Bangladesh
+        &copy; @php echo date('Y'); @endphp <a href="https://tenxorg.com/">TenXOrg.COM</a>, Dhaka, Bangladesh
       </p>
     </center>
   </div>
