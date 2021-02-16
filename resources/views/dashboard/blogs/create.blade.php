f516c0e714e9ea82df594c1323c6896987cca1ce
Azimul Haque <orbachinujbuk@gmail.com>
2021-02-17 00:38:31 +0600

    ASD234

--- resources/views/dashboard/blogs/create.blade.php
+++ resources/views/dashboard/blogs/create.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Add Publication')
+@section('title', 'Add Article')
 
 @section('css')
   <link rel="stylesheet" type="text/css" href="{{ asset('vendor/summernote/summernote.css') }}">
